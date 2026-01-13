425c864a3b64d249bad3d7454403f7c87c37bac0
MorikuMarieke <mariekevdstaaij@gmail.com>
2026-01-13 16:37:51 +0100

    Initial project setup

---
+++ src/main/java/com/moriku/healthcare_file_backend/HealthcareFileBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moriku.healthcare_file_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareFileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareFileBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moriku/healthcare_file_backend/HealthcareFileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moriku.healthcare_file_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareFileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
